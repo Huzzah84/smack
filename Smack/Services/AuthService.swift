db0dddf03263305e0df73bdfc9d6d13aa3129697
Admin <admin@Admins-MacBook-Air.local>
2018-07-31 10:40:23 -0500

    lesson11.

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -48,17 +49,13 @@ class AuthService {
     func registerUser (email: String, password: String, completion: @escaping CompletionHandler) {
     
         let lowerCaseEmail = email.lowercased()
-    
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             
             if response.result.error == nil {
                 completion(true)
@@ -69,5 +66,54 @@ class AuthService {
         }
         
     }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler){
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                    
+//                if let json = response.result.value as? Dictionary<String, Any> {
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+                
+//                // Using SwiftyJSON
+//            THIS IS BROKEN WITHOUT A THROW, TRY, or DO-Catch CODE "I DO NOT KNOW THIS AT THIS TIME"
+//              Also when changes are made the json are no longer recognized
+//              Changed JSON(data:data) to current code as show....seems ok
+                
+                guard let data = response.data else { return }
+                let json = JSON(response.result.value ?? data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                
+            self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
     
 }
